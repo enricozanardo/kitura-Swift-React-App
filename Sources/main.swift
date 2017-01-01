7d777f020c88f59e4513f9870e164485a8fd1916
enrico <ezanardo@onezerobinary.com>
2017-01-01 17:41:33 +0100

    Route the static content to the public folder

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,24 @@
-print("Hello, world!")
+import Kitura
+import HeliumLogger
+
+// Initialize HeliumLogger
+HeliumLogger.use()
+
+// Create a new router
+let router = Router()
+
+// Allow for serving up static files found in the public directory
+router.all("/", middleware: StaticFileServer(path: "./public"))
+
+// Handle HTTP GET requests to /
+// router.get("/") {
+//     request, response, next in
+//     response.send("Hello, World!")
+//     next()
+// }
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
