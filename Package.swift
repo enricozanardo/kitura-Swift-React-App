6b4387daa4850607642ae76a08af397b036b2edf
enrico <ezanardo@onezerobinary.com>
2017-01-02 00:20:37 +0100

    Add Bootstrap support to the project

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "backend",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-	      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4)
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4)
     ]
 )
